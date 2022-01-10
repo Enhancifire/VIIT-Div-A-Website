e13805f793f5948970e4ca6863cf9321849e1253
Faiz Saiyad <81345756+Enhancifire@users.noreply.github.com>
2022-01-10 16:24:22 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,18 +8,22 @@ const RESOURCES = {
 "assets/assets/fonts/FiraSans-Regular.ttf": "895f5b025a6cc4924b263f6beb06c777",
 "assets/FontManifest.json": "45cd61ee95ae30422d7b4f0102c913fc",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "cb280be011dc73bf95d4e95c15de9542",
+"assets/NOTICES": "839b8193272d96ccd963979b8e1f1171",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4faed9bd7c2a88c816da69fd38557d0a",
-"/": "4faed9bd7c2a88c816da69fd38557d0a",
-"main.dart.js": "e783eb9b668d67598774fee51b1e4c55",
-"manifest.json": "6f3caa16b5ebf88efabe5ebc24096285",
-"version.json": "740b782da0c40e723b4fb704cc790280"
+"index.html": "76de315fda754ec384ee8a76ae3aa15f",
+"/": "76de315fda754ec384ee8a76ae3aa15f",
+"main.dart.js": "640bbd2260776727b2c9df235dc01dad",
+"manifest.json": "7997e3242ff0abf090f906cfc7d57a4e",
+"version.json": "60058fdee1028809387a401be82fce08"
 };
 
 // The application shell files that are downloaded before a service worker can
